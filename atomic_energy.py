73c5ec1facfcfadb1b7f4b8a29d447e949b4620c
Ondreiii <frogerr2001@gmail.com>
2023-03-07 16:57:21 +0300

    Добавил атомную энергию пока без дат

---
+++ atomic_energy.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from urllib.parse import urlparse, urljoin
+import html5lib
+from datetime import datetime, timedelta
+
+url = 'https://www.atomic-energy.ru/'
+page = requests.get(url)
+print(page.status_code)
+
+def join(ref):
+    ref = ''.join(ref)
+    href = urljoin(url, ref)
+    return(href)
+
+#def step():
+filteredNews = []
+soup = BeautifulSoup(page.text, "html.parser")
+soup = soup.find("div", {"class": "content rc7"})
+#print (soup)
+allNews = soup.findAll('p')
+for data in allNews:
+    if data.find('a') is not None:
+        filteredNews.append(data.find('a').text)
+        filteredNews.append(join(data.find('a')['href']))
+    #print(filteredNews)
+#return filteredNews
+for data in filteredNews:
+    print(data)
